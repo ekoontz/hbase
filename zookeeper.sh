601fd047a8e205c7b6f914923b38abb906b53087
Eugene Koontz <eugene_koontz@trendmicro.com>
2010-04-30 10:46:29 -0700

    chmod 755; use ~, not /home/ekoontz for portability

--- zookeeper.sh
+++ zookeeper.sh
@@ -1,17 +1,17 @@
 export CLASSPATH=\
-/home/ekoontz/hbase/build/classes:\
-/home/ekoontz/stable/hbase-0.20.3/lib/commons-logging-1.0.4.jar:\
-/home/ekoontz/stable/hbase-0.20.3/lib/hadoop-0.20.1-hdfs127-core.jar:\
-/home/ekoontz/stable/hbase-0.20.3/lib/zookeeper-3.2.2.jar:\
-/home/ekoontz/stable/hbase-0.20.3/lib/log4j-1.2.15.jar
+~/hbase/build/classes:\
+~/stable/hbase-0.20.3/lib/commons-logging-1.0.4.jar:\
+~/stable/hbase-0.20.3/lib/hadoop-0.20.1-hdfs127-core.jar:\
+~/stable/hbase-0.20.3/lib/zookeeper-3.2.2.jar:\
+~/stable/hbase-0.20.3/lib/log4j-1.2.15.jar
 
 export HBASE_CLASSPATH=$CLASSPATH
 
-export HBASE_HOME=/home/ekoontz/hbase
+export HBASE_HOME=~/hbase
 
 export JAVA_HOME=/opt/jdk1.6.0_20/
 
-export PATH=/home/ekoontz/hbase/bin:$PATH
+export PATH=~/hbase/bin:$PATH
 
 hbase zookeeper start 
 
