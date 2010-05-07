701e09fc16f0c662606555d95a04afb3ddbc19a3
Eugene Koontz <eugene_koontz@trendmicro.com>
2010-05-07 13:13:51 -0700

    working on getting regioninfo objects given regionload

--- bin/nsre.rb
+++ bin/nsre.rb
@@ -12,8 +12,18 @@ myadmin = HBaseAdmin.new(org.apache.hadoop.hbase.HBaseConfiguration.new())
 puts myadmin.tableExists 'people'
 puts myadmin.tableExists 'peoplex'
 status = myadmin.getClusterStatus()
-puts status.getHBaseVersion()
-puts status.getServerNames()[0]
+
+for server in status.getServerInfo()
+  # server is a : ...hbase.HServerInfo object
+  puts "regions for : " + server.toString()
+  for region in server.getLoad().getRegionsLoad()
+    # region is a : ...hbase.RegionLoad object. (see 
+    puts("REGION: %s" % [ region.toString() ])
+
+    # get a regionInfo from this regionLoad object.
+  end
+
+end
 
 puts "nsrb.end."
 exit
