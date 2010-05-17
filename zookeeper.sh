62db3b843ebbfa65fc315e422628265c4107eb53
Eugene Koontz <eugene_koontz@trendmicro.com>
2010-05-17 11:19:04 -0700

    fix path

--- zookeeper.sh
+++ zookeeper.sh
@@ -1,9 +1,9 @@
 export CLASSPATH=\
 ~/hbase/build/classes:\
-~/stable/hbase-0.20.3/lib/commons-logging-1.0.4.jar:\
-~/stable/hbase-0.20.3/lib/hadoop-0.20.1-hdfs127-core.jar:\
-~/stable/hbase-0.20.3/lib/zookeeper-3.2.2.jar:\
-~/stable/hbase-0.20.3/lib/log4j-1.2.15.jar
+~/hbase/lib/commons-logging-1.0.4.jar:\
+~/hbase/lib/hadoop-0.20.1-hdfs127-core.jar:\
+~/hbase/lib/zookeeper-3.2.2.jar:\
+~/hbase/lib/log4j-1.2.15.jar
 
 export HBASE_CLASSPATH=$CLASSPATH
 
