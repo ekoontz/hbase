e9d9f54069c4b706ee2c4a82b62536c2f922ca42
Eugene Koontz <eugene_koontz@trendmicro.com>
2010-05-17 10:23:46 -0700

    last changes to 0.20.3.ekoontz before switching to 0.21.ekoontz

--- hbase-2486/src/org/apache/hadoop/hbase/master/TestNSRE.java
+++ hbase-2486/src/org/apache/hadoop/hbase/master/TestNSRE.java
@@ -22,12 +22,51 @@ import org.apache.hadoop.hbase.util.Writables;
 import org.apache.hadoop.util.StringUtils;
 
 public class TestNSRE extends HBaseClusterTestCase {
-	  public void testNSRE()
-	   throws Exception {
-	    HTable meta = new HTable(HConstants.META_TABLE_NAME);
-	    HMaster master = this.cluster.getMaster();
-	    HServerAddress address = master.getMasterAddress();
-	    System.out.println("ekoontzdebug: " + address.toString());
-	    assertEquals(42,42);
-	   }
+  public void testNSRE()
+    throws Exception {
+
+     HTable meta = new HTable(HConstants.META_TABLE_NAME);
+     HMaster master = this.cluster.getMaster();
+     HServerAddress address = master.getMasterAddress();
+     HTableDescriptor tableDesc = new HTableDescriptor(Bytes.toBytes("_MY_TABLE_"));
+     HTableDescriptor metaTableDesc = meta.getTableDescriptor();
+     // master.regionManager.onlineMetaRegions already contains first .META. region at key Bytes.toBytes("")
+     byte[] startKey0 = Bytes.toBytes("f");
+     byte[] endKey0 = Bytes.toBytes("h");
+     HRegionInfo regionInfo0 = new HRegionInfo(tableDesc, startKey0, endKey0);
+
+     // 1st .META. region will be something like .META.,,1253625700761
+     HRegionInfo metaRegionInfo0 = new HRegionInfo(metaTableDesc, Bytes.toBytes(""), regionInfo0.getRegionName());
+     MetaRegion meta0 = new MetaRegion(address, metaRegionInfo0);
+   
+     byte[] startKey1 = Bytes.toBytes("j");
+     byte[] endKey1 = Bytes.toBytes("m");
+     HRegionInfo regionInfo1 = new HRegionInfo(tableDesc, startKey1, endKey1);
+     // 2nd .META. region will be something like .META.,_MY_TABLE_,f,1253625700761,1253625700761 
+     HRegionInfo metaRegionInfo1 = new HRegionInfo(metaTableDesc, regionInfo0.getRegionName(), regionInfo1.getRegionName());
+     MetaRegion meta1 = new MetaRegion(address, metaRegionInfo1);
+
+
+     // 3rd .META. region will be something like .META.,_MY_TABLE_,j,1253625700761,1253625700761
+     HRegionInfo metaRegionInfo2 = new HRegionInfo(metaTableDesc, regionInfo1.getRegionName(), Bytes.toBytes(""));
+     MetaRegion meta2 = new MetaRegion(address, metaRegionInfo2);
+
+     byte[] startKeyX = Bytes.toBytes("h");
+     byte[] endKeyX = Bytes.toBytes("j");
+     HRegionInfo regionInfoX = new HRegionInfo(tableDesc, startKeyX, endKeyX);
+   
+   
+     master.regionManager.offlineMetaRegion(startKey0);
+     master.regionManager.putMetaRegionOnline(meta0);
+     master.regionManager.putMetaRegionOnline(meta1);
+     master.regionManager.putMetaRegionOnline(meta2);
+
+     assertEquals(metaRegionInfo1.getStartKey(), master.regionManager.getFirstMetaRegionForRegion(regionInfoX).getStartKey());
+     assertEquals(metaRegionInfo1.getRegionName(), master.regionManager.getFirstMetaRegionForRegion(regionInfoX).getRegionName());
+
+
+
+
+
+  }
 }
