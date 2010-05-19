45bc4cf5be1422038606ca65df8ec1f6fdef20ed
Eugene Koontz <eugene_koontz@trendmicro.com>
2010-05-18 17:43:37 -0700

    cleanup

--- bin/zookeeper.sh
+++ bin/zookeeper.sh
@@ -1,9 +1,6 @@
-export CLASSPATH=\
-/home/ekoontz/hbase/core/target/classes:\
-/home/ekoontz/stable/hbase-0.20.3/lib/commons-logging-1.0.4.jar:\
-/home/ekoontz/stable/hbase-0.20.3/lib/hadoop-0.20.1-hdfs127-core.jar:\
-/home/ekoontz/stable/hbase-0.20.3/lib/zookeeper-3.2.2.jar:\
-/home/ekoontz/stable/hbase-0.20.3/lib/log4j-1.2.15.jar
+#!/bin/sh 
+
+export CLASSPATH=/home/ekoontz/hbase/core/target/classes
 
 export HBASE_CLASSPATH=$CLASSPATH
 
