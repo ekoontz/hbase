1e9f9288fdcb415351f1e34d69ae8670c4f84ab5
Eugene Koontz <eugene_koontz@trendmicro.com>
2010-05-21 10:43:38 -0700

    removing stuff from TestNSRE and starting from just a single test example

--- src/test/java/org/apache/hadoop/hbase/regionserver/TestNSRE.java
+++ src/test/java/org/apache/hadoop/hbase/regionserver/TestNSRE.java
@@ -182,51 +182,6 @@ public class TestNSRE extends HBaseTestCase {
       }
     }
   }
-
-  public void testIndexesScanWithOneDeletedRow() throws IOException {
-    byte[] tableName = Bytes.toBytes("testIndexesScanWithOneDeletedRow");
-    byte[] family = Bytes.toBytes("family");
-
-    //Setting up region
-    String method = "testIndexesScanWithOneDeletedRow";
-    initHRegion(tableName, method, new HBaseConfiguration(), family);
-
-    Put put = new Put(Bytes.toBytes(1L));
-    put.add(family, qual1, 1L, Bytes.toBytes(1L));
-    region.put(put);
-
-    region.flushcache();
-
-    Delete delete = new Delete(Bytes.toBytes(1L), 1L, null);
-    //delete.deleteColumn(family, qual1);
-    region.delete(delete, null, true);
-
-    put = new Put(Bytes.toBytes(2L));
-    put.add(family, qual1, 2L, Bytes.toBytes(2L));
-    region.put(put);
-
-    Scan idxScan = new Scan();
-    idxScan.addFamily(family);
-    idxScan.setFilter(new FilterList(FilterList.Operator.MUST_PASS_ALL,
-      Arrays.<Filter>asList(new SingleColumnValueFilter(family, qual1,
-        CompareFilter.CompareOp.GREATER_OR_EQUAL,
-        new BinaryComparator(Bytes.toBytes(0L))),
-        new SingleColumnValueFilter(family, qual1,
-          CompareFilter.CompareOp.LESS_OR_EQUAL,
-          new BinaryComparator(Bytes.toBytes(3L)))
-      )));
-    InternalScanner scanner = region.getScanner(idxScan);
-    List<KeyValue> res = new ArrayList<KeyValue>();
-
-    //long start = System.nanoTime();
-    while (scanner.next(res)) ;
-    //long end = System.nanoTime();
-    //System.out.println("memStoreEmpty=" + memStoreEmpty + ", time=" + (end - start)/1000000D);
-    assertEquals(1L, res.size());
-
-  }
-
-
   
   private void putData(int startRow, int numRows, byte [] qf,
       byte [] ...families)
