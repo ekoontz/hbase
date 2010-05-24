9c9bea6510528af925f9ee1faac3aeded36494ea
Eugene Koontz <eugene_koontz@trendmicro.com>
2010-05-24 11:50:55 -0700

    working on first test (NSRE for region in transition)

--- src/test/java/org/apache/hadoop/hbase/master/TestNSREHandling.java
+++ src/test/java/org/apache/hadoop/hbase/master/TestNSREHandling.java
@@ -31,7 +31,26 @@ import org.apache.hadoop.hbase.util.Bytes;
 public class TestNSREHandling extends HBaseClusterTestCase {
    public void testHandleNSREInTransition()
    throws Exception {
-       assertEquals(42,42);
+
+     HTable meta = new HTable(HConstants.META_TABLE_NAME);
+     HMaster master = this.cluster.getMaster();
+     HServerAddress address = master.getMasterAddress();
+     HTableDescriptor tableDesc = new HTableDescriptor(Bytes.toBytes("_MY_TABLE_"));
+
+     // master.regionManager.onlineMetaRegions already contains first .META. region at key Bytes.toBytes("")
+     byte[] startKey0 = Bytes.toBytes("f");
+     byte[] endKey0 = Bytes.toBytes("h");
+     HRegionInfo regionInfo0 = new HRegionInfo(tableDesc, startKey0, endKey0);
+
+     // get region info for _MY_TABLE_.
+     // 1. Put a region r (regionInfo0) into transition.
+
+     // 2. Put a HMsg with a NSRE(r) in master message queue.
+
+     // 3. See if server handles NSRE(r) correctly.
+
+
+     assertEquals(42,42);
    }
 
    public void testHandleNSREOnDifferentRS()
