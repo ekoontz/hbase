95a2bed917d00f404c76124bd6fb841db1cd738a
Eugene Koontz <ekoontz@hiro-tan.org>
2010-08-12 18:16:56 -0700

    moving coprocessor code into coprocessor area

---
+++ src/main/java/org/apache/hadoop/hbase/Permissions.java
@@ -0,0 +1,7 @@
+package org.apache.hadoop.hbase.permissions;
+
+import org.apache.hadoop.hbase.coprocessor.Coprocessor;
+import org.apache.hadoop.hbase.coprocessor.RegionObserver;
+
+public class Permissions implements Coprocessor, RegionObserver {
+}
